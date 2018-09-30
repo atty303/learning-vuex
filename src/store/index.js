a832c969c47ef44de6b667ee2b1f645f7ac4b4a7
Koji AGAWA <atty303@gmail.com>
2018-09-30 21:59:07 +0900

    ch2-7

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
 
+import shop from '@/api/shop'
+import { rejects } from 'assert';
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -15,8 +18,13 @@ export default new Vuex.Store({
   },
 
   actions: {
-    fetchProducts() {
-
+    fetchProducts({commit}) {
+      return new Promise((resolve, reject) => {
+        shop.getProducts(products => {
+          commit('setProducts', products)
+          resolve()
+        })
+      })
     }
   },
 
